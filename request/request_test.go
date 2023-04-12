9b346851bfb286d0b9074212225fd4fc7a17fd17
marstion <51946657+marstion@users.noreply.github.com>
2023-04-12 16:12:21 +0800

    Update request_test.go

--- request/request_test.go
+++ request/request_test.go
@@ -1,11 +1,10 @@
 package request
 
 import (
-	"log"
 	"testing"
 )
 
 func TestRequest(t *testing.T) {
-	response := Request("GET", "http://httpbin.org/ip", nil, "")
-	log.Printf("response: %#v\n", string(response))
+	statusCode, response := Request("GET", "http://httpbin.org/ip", nil, "")
+	t.Log(statusCode, string(response))
 }
